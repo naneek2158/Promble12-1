66ff8fe1fa42fe17d8d69bd966ae5e5ed9c8bd37
naneek2158 <keenanv4152@sd61learn.ca>
2016-02-04 13:14:16 -0800

    Update problem12-1.py

--- problem12-1.py
+++ problem12-1.py
@@ -7,11 +7,11 @@ recorded_speed = input("Enter the recorded speed of the car: ")
 
 speed_difference = recorded_speed - speed_limit
 
-if speed_difference > 31:
+if speed_difference > 30:
     print("You are speeding and your fine is $500")
-elif speed_difference > 21:
+elif speed_difference > 20:
     print("You are speeding and your fine is $270")
-elif speed_difference > 1:
+elif speed_difference > 0:
     print("You are speeding and your fine is $100")
 else:
     print("Congratulations. You are within the speed limit!")
